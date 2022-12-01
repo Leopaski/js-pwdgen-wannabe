8d3366520240806c7e3ad7020648177aca6d2983
Leopaski <leonardo.pascale@gmail.com>
2022-12-01 16:13:09 +0100

    finito

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-let nome
+let name = prompt("inserisci il tuo nome")
+let surname = prompt ("inserisci il tuo cognome")
+let color = prompt ("inserisci il tuo colore preferito")
+const number = 21
+
+document.getElementById ("password").innerHTML = name + surname + color + number 
